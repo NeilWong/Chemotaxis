09b0384547104d234feab66ea615c78befd6681d
NeilWong <nwong1250@gmail.com>
2014-09-25 11:30:40 -0700

    NeilWong

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 Bacteria [] colony;
- void setup()   
+ public void setup()   
  {     
  	size(300,300);
  	//initialize bacteria variables here   
@@ -9,7 +25,7 @@ Bacteria [] colony;
  		colony[i] = new Bacteria(150,150);
  	}
  }   
- void draw()   
+ public void draw()   
  {   
  	background(255,255,255);
  	//move and show the bacteria   
@@ -35,7 +51,7 @@ Bacteria [] colony;
  		dir = 5;
  		dir2 = 2;	
  	}
- 	void show()
+ 	public void show()
  	{
  		int myColor = (int)(Math.random()*255);
  		int siz = (int)(Math.random()*18)-9;
@@ -46,13 +62,13 @@ Bacteria [] colony;
  		}
  		//if(mousePressed)
  	}
- 	void move() 
+ 	public void move() 
  	{
  		
  		germX = germX + (int)(Math.random()*dir)-dir2;
  		germY = germY + (int)(Math.random()*dir)-dir2;
  	}
- 	void mousePressed()
+ 	public void mousePressed()
  	{
  	noStroke();
  	fill(0,255,0);
@@ -150,3 +166,12 @@ Bacteria [] colony;
     
  
 //Math.random()*255,Math.random()*255,Math.random()*255
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
